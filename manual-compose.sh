77734598a3ac1543b60d9451b2ca0186129e9983
dhkdtld37 <hyeonnls96@naver.com>
2024-02-20 13:53:28 +0900

    clone manual.sh file

--- manual-compose.sh
+++ manual-compose.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
 # var
+BLOG_1="pysatellite-blog-a.internal"
+BLOG_2="pysatellite-blog-b.internal"
+BLOG_1_IMG_NAME="blog-1-img"
+BLOG_2_IMG_NAME="blog-2-img"
 
 # start
+# sl -aF
 figlet -f bubble start
 
-# rm & rmi
-figlet stop & rm & rmi
+#############################
+figlet remove all
 sudo docker stop $(sudo docker ps -q)
 sudo docker rm $(sudo docker ps -a -q)
 sudo docker rmi $(sudo docker images -q)
+sudo docker network rm blog-net
 
-# build
+#############################
+# https://docs.docker.com/engine/reference/commandline/image_build/
 figlet build
-sudo docker images build -t blog-a -f docker/blog-a/Dockerfile docker/blog-a
-sudo docker images build -t blog-b -f docker/blog-a/Dockerfile docker/blog-b
-sudo docker images build -t lb -f docker/lb/Dockerfile docker/lb
+sudo docker image build -t $BLOG_1_IMG_NAME -f docker/blog-a/Dockerfile docker/blog-a
+sudo docker image build -t $BLOG_2_IMG_NAME -f docker/blog-b/Dockerfile docker/blog-b
+sudo docker image build -t blog-lb -f docker/lb/Dockerfile docker/lb
 
-# docker network
-sudo docker network create 2402240
-sudo docker network connect 240220 awsgoo-nginx-proxy-1
-sudo docker network connect 240220 awsgoo-blog-1
-sudo docker network connect 240220 awsgoo-blog-2
-sudo docker network inspect 2402240
+#############################
+# https://docs.docker.com/engine/reference/commandline/network_create/
+figlet create network
+sudo docker network create -d bridge blog-net
 
-
-# run
+#############################
+# https://docs.docker.com/engine/reference/commandline/container_run/
 figlet run
-sudo docker run -d blog-a-1 -p 8001:80 blog-a
-sudo docker run -d blog-b-1 -p 8002:80 blog-b
-sudo docker run -d lb-1 -p 8000:80 lb
+sudo docker container run -d --name $BLOG_1 -p 8001:80 --network blog-net $BLOG_1_IMG_NAME
+sudo docker container run -d --name $BLOG_2 -p 8002:80 --network blog-net $BLOG_2_IMG_NAME
+sudo docker container run -d --name blog-lb-1 -p 8000:80 --network blog-net blog-lb
 
-# ps
+#############################
 figlet ps
-sudo docker ps -a
-sudo docker network ps
+sudo docker container ps
+
+figlet -f TEST
+curl http://localhost:8000 | grep h1
+curl http://localhost:8000 | grep h1
 
 # end
-sl -a
+figlet -f bubble END
